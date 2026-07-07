53c491255f242e645fe4d87de510af21893aabd4
TK-from-TSK <tarunkarthik.rajesh@gmail.com>
2026-07-07 19:36:34 +0530

    Variables and input.

---
+++ Variables.py
@@ -0,0 +1,14 @@
+x = 4
+y = 2
+Tsk = 4 * 2
+print(x)
+print(y)
+print(Tsk)
+
+print("Now, we will ask your name. Please enter your name and cooperate..")
+
+z = input("Please enter your name.")
+
+
+
+print("Hello.Welcome to Codingal",z)
