0d510e91c7e1c63f277f75261fb426a51d64a556
TK-from-TSK <tarunkarthik.rajesh@gmail.com>
2026-07-12 09:37:29 +0530

    Data_type.py

---
+++ Data_Types.py
@@ -0,0 +1,23 @@
+T = 42
+S = 42.3
+K = "TSK LABS!"
+
+
+L = True
+A = False
+B = "HELLO!"
+S = "WORLD!"
+
+print(type(T))
+print()
+print(type(S))
+print()
+print(type(K))
+print()
+print(type(L))
+print()
+print(type(A))
+print()
+print(type(B))
+print()
+print(type(S))
