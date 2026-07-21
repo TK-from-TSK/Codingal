b13229869a2c15977b82d425371a07416f1cdbfa
TK-from-TSK <tarunkarthik.rajesh@gmail.com>
2026-07-21 20:25:02 +0530

    Big or small

---
+++ Big_or_small.py
@@ -0,0 +1,11 @@
+x = float(input("Enter a number : "))
+if (x < 15):
+    print ("x is smaller than 15")
+    print ("x is in if Block")
+elif (x > 15):
+    print ("x is greater than 15")
+    print ("x is in elif(1) Block")
+elif (x == 15):
+    print ("x is equal to 15")
+    print ("x is in elif(2) Block")
+
