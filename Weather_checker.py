70da147ac230bd4ac4fd4d3ea582f1ae39a89e7c
TK-from-TSK <tarunkarthik.rajesh@gmail.com>
2026-07-21 20:10:42 +0530

    Weather

---
+++ Weather_checker.py
@@ -0,0 +1,7 @@
+weather = int(input("Enter the current temperature in Celsius: "))
+
+if weather < 35:
+    print("It's winter.We recommend you to wear warm clothes and stay hydrated.")
+
+else:
+    print("It's summer.We recommend you to wear light clothes and stay hydrated.")
