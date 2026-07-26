2794931a50e29b9760f2562b490036562726af69
TK-from-TSK <tarunkarthik.rajesh@gmail.com>
2026-07-26 10:25:49 +0530

    Alphabet checker

---
+++ Alphabet_checker.py
@@ -0,0 +1,7 @@
+random = input("Enter: ")
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+if random == alphabet:
+    print("This is an alphabet", random)
+else:
+    print("This is not an alphabet", random)
