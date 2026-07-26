83b58420d42078ed503505375d19ddbc35aebc60
TK-from-TSK <tarunkarthik.rajesh@gmail.com>
2026-07-26 09:32:45 +0530

    And _or operators

---
+++ And_or_operators.py
@@ -0,0 +1,22 @@
+x = 10
+y = 42
+c = 0
+
+number = 10,42,0
+
+if x and y and c:
+    print("All these numbers are Boolean.", number)
+
+else:
+    print("One of them is Boolean." ,number)
+
+t = 100
+s = -10
+k = 0
+numb = 100,-10,0
+total = 100
+
+if t>k or s>k:
+    print("This number is greater.", total)
+else:
+    print("None of these numbers are greater than each other.")
