98979c076f874fb206295a3d941c057349ee09a7
TK-from-TSK <tarunkarthik.rajesh@gmail.com>
2026-09-01 19:20:04 +0530

    Character occurence

---
+++ Character_occurence.py
@@ -0,0 +1,12 @@
+string = input("Enter your word: ")
+
+char = input("Enter your character: ")
+i = 0
+count = 0
+
+while (i < len(string)):
+    if (string[i] == char):
+        count = count + 1
+    i = i + 1
+
+print("This character ", char, "has occured", count, "times in the word", string)
