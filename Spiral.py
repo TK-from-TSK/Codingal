d25996cf9389972887ad9f65f4fdabfb30daa098
TK-from-TSK <tarunkarthik.rajesh@gmail.com>
2026-09-20 10:11:57 +0530

    Spiral

--- Spiral.py
+++ Spiral.py
@@ -4,3 +4,15 @@ my_wn.bgcolor("light blue")
 my_wn.title("Spiral")
 
 my_pen = turtle.Turtle()
+
+size = 0
+colors = ["red", "purple", "blue", "green", "orange", "yellow"]
+
+while True:
+    for i in range(4):
+        my_pen.pencolor(colors[i])
+        my_pen.fd(size + 1)
+        my_pen.left(90)
+        size = size-5
+
+    size = size + 1
